e0313c06b31ce88fde363bfd00c893b8eceeebaa
Grant Spilsbury <grantspilsbury@gmail.com>
2018-02-16 07:25:20 +1100

    Remove unwanted method bind

--- client/src/components/Workspace.jsx
+++ client/src/components/Workspace.jsx
@@ -15,7 +15,6 @@ class Workspace extends Component {
     super(props);
     // const port = process.env.PORT || '3000' or window.location.hostname || 'https://tutorhq.herokuapp.com/' localhost:3000;
     this.socket = io.connect('https://tutorhq.herokuapp.com/');
-    this.handleClick = this.handleClick.bind(this);
     this.handleEndSession = this.handleEndSession.bind(this);
     this.startVideo = this.startVideo.bind(this);
     this.toggleVideo = this.toggleVideo.bind(this);
